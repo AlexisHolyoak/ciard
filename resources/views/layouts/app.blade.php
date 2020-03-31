666b180b9f5d3dfb9dfeda8227fc21c09b694745
AlexisHolyoak <peraltaholyoak.aj@gmail.com>
2020-03-31 16:18:47 -0500

    changed colors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm ">
+        <nav class="navbar navbar-expand-md navbar-dark nav-color shadow-sm ">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'C.I.A.R.D') }}
--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,6 +1,22 @@
  <!-- Fonts -->
- 
+
 
  <!-- Styles -->
- 
+
  @yield('style')
+
+ <style>
+     .card-header{
+         background-color: #80AEAB;
+         color: white;
+     }
+     .card-body{
+         background-color: #EDF1F0;
+     }
+     .nav-color{
+         background-color: #0078A7;
+     }
+     .nav-item{
+         : white;
+     }
+ </style>
