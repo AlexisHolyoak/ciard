979434531bff753a034481f8ecb99869833cd0b6
AlexisHolyoak <peraltaholyoak.aj@gmail.com>
2020-03-31 17:03:23 -0500

    changed main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'C.I.A.R.D.') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <!-- Styles -->
         <style>
             html, body, .content {
@@ -91,7 +91,7 @@
                 </div>
             </div>
         </nav>
-        <div class="container">
+        <div class="container position-ref full-height ">
             <div class="row justify-content-center">
                 <div class="title m-b-md">
                     C.I.A.R.D.
@@ -110,6 +110,6 @@
             </div>
         </div>
     </div>
-    <script src="{{ asset('js/app.js') }}" ></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     </body>
 </html>
